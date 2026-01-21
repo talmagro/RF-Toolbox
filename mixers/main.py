6419929b2c309d2ffb83d0827524f3ef34ac576d
talmagro <tonialrod9@gmail.com>
2026-01-21 22:52:58 +0100

    Format change

--- mixers/main.py
+++ mixers/main.py
@@ -14,7 +14,7 @@ if_signal = np.cos(2 * np.pi * f_if * t)
 lo_signal = np.cos(2 * np.pi * f_lo * t)
 
 # Mixer (Multiplication)
-mixed_signal = if_signal * lo_signalh
+mixed_signal = if_signal * lo_signal
 
 # FFT helper
 def compute_fft(signal, fs):
